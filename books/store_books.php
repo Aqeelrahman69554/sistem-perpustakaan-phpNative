95898fe41a99154e607befff8b3e01ef67f9d6f6
aqeel <haikalrahman69612@example.com>
2026-06-13 05:43:49 +0700

    feat: menambahkan pengkondisian di store_books untuk code buku

--- books/store_books.php
+++ books/store_books.php
@@ -11,6 +11,17 @@ $year = $_POST['relase_year'];
 $stok = $_POST['stock'];
 $description = $_POST['description'];
 
+$cek_query = "SELECT book_code FROM books WHERE book_code = '$book_code'";
+$cek_result = mysqli_query($connect,$cek_query);
+
+if(mysqli_num_rows($cek_result) > 0){
+    echo "<script>
+    alert('gagal menambahkan kode buku!, karena kode buku sudah digunakan oleh buku lain');
+    window.history.back();
+    </script>";
+    exit;
+}
+
 
 $query = "INSERT INTO books(book_code,title, category_id, author, publisher, relase_year, stock, description) VALUES('$book_code','$title', '$category_id', '$author', '$publisher', '$year', '$stok', '$description')";
 
