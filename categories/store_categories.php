79639397d4c45fffd3d985137db7fb869d938beb
aqeel <haikalrahman69612@example.com>
2026-06-13 07:44:26 +0700

    feat:menambahkan pengkondisian untuk nama kategori supaya tidak bisa sama untuk kategori yang sudah terdaftar

--- categories/store_categories.php
+++ categories/store_categories.php
@@ -3,12 +3,23 @@ require_once __DIR__ . '/../connection.php';
 
 $category_name = $_POST['category_name'];
 
+$cek_query = "SELECT id FROM categories WHERE category_name = '$category_name'";
+$cek_result = mysqli_query($connect, $cek_query);
+
+if(mysqli_num_rows($cek_result) > 0){
+    echo "<script>
+    alert('gagal menambahkan! karena nama kategori sudah digunakan oleh kategori lain');
+    window.history.back();
+    </script>";
+    exit;
+}
+
 $query = "INSERT INTO categories (category_name) VALUES('$category_name')";
 
 $result = mysqli_query($connect, $query);
 
 if ($result) {
-    
+
     header("location: index_categories.php");
 } else {
     echo "penyimpanan gagal";
--- categories/update_categories.php
+++ categories/update_categories.php
@@ -6,6 +6,17 @@ $id = $_POST['id'];
 
 $category_name = $_POST['category_name'];
 
+$cek_query = "SELECT id FROM categories WHERE category_name = '$category_name' AND id != '$id'";
+$cek_result = mysqli_query($connect, $cek_query);
+
+if(mysqli_num_rows($cek_result) > 0){
+    echo "<script>
+    alert('gagal merubah data kategori! karena nama kategori sudah digunakan oleh kategori lain');
+    window.history.back();
+    </script>";
+    exit;
+}
+
 $query = "UPDATE categories
 SET category_name = '$category_name'
 WHERE id = '$id'";
