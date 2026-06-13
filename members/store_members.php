5fe413ba1ffb7497954b713619866b309e11ce5b
aqeel <haikalrahman69612@example.com>
2026-06-13 07:13:44 +0700

    feat: menambahkan pengkondisian di edit,store, update untuk id anggota dan menambahkan kolom id

--- members/store_members.php
+++ members/store_members.php
@@ -1,13 +1,24 @@
 <?php
 require_once __DIR__ . '/../connection.php';
 
-$id = $_POST['id_anggota'];
+$id_anggota = $_POST['id_anggota'];
 $fullname = $_POST['nama'];
 $gender = $_POST['jk'];
 $phone = $_POST['telp'];
 $address = $_POST['alamat'];
 
-$query_members = "INSERT INTO members(id_anggota,full_name, gender, phone, address) VALUES('$id','$fullname', '$gender', '$phone', '$address')";
+$cek_query = "SELECT id_anggota FROM members WHERE id_anggota = '$id_anggota'";
+$cek_result = mysqli_query($connect,$cek_query);
+
+if(mysqli_num_rows($cek_result) > 0){
+    echo "<script>
+    alert('Gagal menambahkan, karena nomor anggota sudah terdaftar');
+    window.history.back();
+    </script>";
+    exit;
+}
+
+$query_members = "INSERT INTO members(id_anggota,full_name, gender, phone, address) VALUES('$id_anggota','$fullname', '$gender', '$phone', '$address')";
 
 $result = mysqli_query($connect, $query_members);
 
--- members/update_members.php
+++ members/update_members.php
@@ -2,12 +2,24 @@
 
 require_once __DIR__ . '/../connection.php';
 
-$id = $_POST['id_anggota'];
+$id = $_POST['id'];
+$id_anggota = $_POST['id_anggota'];
 
 $name = $_POST['nama'];
 $gender = $_POST['jk'];
 $phone = $_POST['telp'];
 $address = $_POST['alamat'];
+
+$cek_query = "SELECT id_anggota FROM members WHERE id_anggota = '$id_anggota' AND id != '$id' ";
+$cek_result = mysqli_query($connect, $cek_query);
+
+if(mysqli_num_rows($cek_result) > 0){
+    echo "<script>
+    alert('gagal menambahkan! karena ID Anggota sudah digunakan oleh anggota lain');
+    window.history.back();
+    </script>";
+    exit;
+}
 
 $query_update_members = "UPDATE members SET
 id_anggota = '$id',
